23a8a1a04eac1bf3727fea5c1055f1ded73896a7
RaynaRR <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>
2021-08-21 16:54:05 +0800

    hello~

---
+++ src/views/hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>hello~~~</div>
+</template>
+
+<script>
+    export default {
+        name: "hello",
+        props: {},
+        components: {},
+        mounted: {},
+        data() {
+            return {}
+        },
+        methods: {},
+    }
+</script>
+
+<style scoped>
+
+</style>
